94aff76d873f4c2dee65f8c6aeff8a741734442f
johan sebastian reyes <reyesmontoyamonor@gmail.com>
2026-08-30 16:17:03 -0500

    nuevo diseño

--- js/config/constants.js
+++ js/config/constants.js
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = '593998968151';
+export const WHATSAPP_NUMBER = '593987426825';
 export const WHATSAPP_MESSAGE = 'Hola, quiero más información sobre sus servicios';
 export const CATALOG_TITLE = 'Catálogo de productos';
 export const GALERIA_TITLE = 'Galería';
